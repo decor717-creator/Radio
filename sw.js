ef6e81dd44ba22772e6bf69fa964277bacee8cd2
decor717-creator <decor717@gmail.com>
2026-09-21 10:21:29 +0300

    Refresh PWA cache for stream watchdogs

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'my-radio-shell-v8';
+const CACHE = 'my-radio-shell-v9';
 const SHELL = [
-  './', './index.html', './styles.css?v=4', './app.js?v=8',
+  './', './index.html', './styles.css?v=4', './app.js?v=9',
   './manifest.webmanifest', './icon-192.png', './icon-512.png'
 ];
 
