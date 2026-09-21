95925e1d9c64b56f96868c469a9121f71437b340
decor717-creator <decor717@gmail.com>
2026-09-21 09:25:43 +0300

    Refresh PWA cache for search failover

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'my-radio-shell-v6';
+const CACHE = 'my-radio-shell-v7';
 const SHELL = [
-  './', './index.html', './styles.css?v=4', './app.js?v=6',
+  './', './index.html', './styles.css?v=4', './app.js?v=7',
   './manifest.webmanifest', './icon-192.png', './icon-512.png'
 ];
 
