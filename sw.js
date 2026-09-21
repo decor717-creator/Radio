c480b5d889008bdab7bf897569420e87cc218e8d
decor717-creator <decor717@gmail.com>
2026-09-21 09:54:36 +0300

    Refresh PWA cache for playback fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'my-radio-shell-v7';
+const CACHE = 'my-radio-shell-v8';
 const SHELL = [
-  './', './index.html', './styles.css?v=4', './app.js?v=7',
+  './', './index.html', './styles.css?v=4', './app.js?v=8',
   './manifest.webmanifest', './icon-192.png', './icon-512.png'
 ];
 
